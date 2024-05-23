429be3321c590d22962ab721cc0fdad96adc2f03
baielbekenov <baelbekbolotovb@gmail.com>
2024-05-23 18:24:45 +0600

    'dd'

--- main/settings.py
+++ main/settings.py
@@ -122,12 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'toursite-staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, "toursite-staticfiles")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = 'media/'
 
-STATIC_ROOT = ''
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
